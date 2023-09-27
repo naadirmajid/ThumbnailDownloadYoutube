1e55f7e52694c770b653ba3416f90779d7d6acc3
unknown <naadirmajid>
2023-09-27 19:41:06 +0100

    resorde problem

--- vercel_flask.py
+++ vercel_flask.py
@@ -1,6 +1,8 @@
 # Import necessary modules
 from flask import Flask, render_template, request
-from your_flask_app_file import app as flask_app  # Assuming your Flask app is named 'app' and it's in 'your_flask_app_file.py'
+from app import app as flask_app
+
+from youtube_thumbnail_webapp import app as flask_app  # Assuming your Flask app is named 'app' and it's in 'your_flask_app_file.py'
 
 # Create the WSGI application object
 app = flask_app
